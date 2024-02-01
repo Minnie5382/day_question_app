4cd693354fe57696bc1d76f6868456deb52ac513
Minn <radic7700@gmail.com>
2024-02-01 23:01:05 +0900

    initial commit

---
+++ backend/src/main/java/site/dayquestion/day_question/DayQuestionApplication.java
@@ -0,0 +1,13 @@
+package site.dayquestion.day_question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayQuestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayQuestionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/site/dayquestion/day_question/DayQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dayquestion.day_question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayQuestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
